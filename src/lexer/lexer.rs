85be3e9e6c6ec6f93b821dfd6b5ee41a1d488cde
Vlad2001_MFS <kopytin.vladislav2001@yandex.ru>
2020-09-26 17:12:06 +0300

    refactor: split process function into some parts

--- src/lexer/lexer.rs
+++ src/lexer/lexer.rs
@@ -17,9 +17,8 @@ const HEX_CHARS: [char; 16] = [
 #[derive(Debug)]
 pub enum Token {
     Identifier(String),
-    ShortLiteral(String),
-    Int(i64),
-    Real(f64),
+    String(String),
+    Number(f64),
     Other(String),
 
     // Keywords
@@ -74,128 +73,19 @@ impl Lexer {
             self.stream.skip();
             
             if self.stream.look_for("--[[", true) {
-                let mut depth = 1;
-                while depth > 0 {
-                    if self.stream.look_for("--]]", true) {
-                        depth -= 1;
-                    }
-                    else if self.stream.look_for("--[[", true) {
-                        depth += 1;
-                    }
-                    self.stream.next(false);
-                }
+                self.process_block_comment();
             }
             else if self.stream.look_for("--", true) {
-                while self.stream.last_char() != '\n' {
-                    self.stream.next(false);
-                }
+                self.process_line_comment();
             }
             else if self.stream.last_char() == '\'' || self.stream.last_char() == '"' {
-                let mut short_literal = String::new();
-                while self.stream.next(false) {
-                    if self.stream.last_char() == '\\' {
-                        self.stream.next(false);
-                        short_literal.push(self.stream.last_char());
-                    }
-                    else if self.stream.last_char() == '\'' || self.stream.last_char() == '"' {
-                        self.stream.next(false);
-                        break;
-                    }
-                    else {
-                        short_literal.push(self.stream.last_char());
-                    }
-                }
-                self.tokens.push(Token::ShortLiteral(short_literal));
+                self.process_short_string_literal();
             }
             else if self.stream.last_char().is_alphabetic() || self.stream.last_char() == '_' {
-                let mut name = self.stream.last_char().to_string();
-                while self.stream.next(false) {
-                    if self.stream.last_char().is_alphanumeric() || self.stream.last_char() == '_' {
-                        name.push(self.stream.last_char());
-                    }
-                    else {
-                        break;
-                    }
-                }
-
-                self.tokens.push(match name.as_str() {
-                    "function" => Token::Function,
-                    "end" => Token::End,
-
-                    "if" => Token::If,
-                    "then" => Token::Then,
-                    "else" => Token::Else,
-                    "elseif" => Token::ElseIf,
-
-                    "for" => Token::For,
-                    "while" => Token::While,
-                    "do" => Token::Do,
-                    "repeat" => Token::Repeat,
-                    "until" => Token::Until,
-
-                    "or" => Token::Or,
-                    "and" => Token::And,
-                    "not" => Token::Not,
-
-                    "goto" => Token::Goto,
-                    "break" => Token::Break,
-                    "return" => Token::Return,
-
-                    "in" => Token::In,
-                    "local" => Token::Local,
-                    "nil" => Token::Nil,
-                    "true" => Token::True,
-                    "false" => Token::False,
-
-                    _ => Token::Identifier(name),
-                });
+                self.process_identifier();
             }
             else if self.stream.last_char().is_digit(10) || (self.stream.last_char() == '-' && self.stream.look(1).unwrap_or('\0').is_digit(10)) {
-                if self.stream.look(1).unwrap_or('\0').to_ascii_lowercase() == 'x' {
-                    self.stream.next(false);
-
-                    let mut number = String::new();
-                    while self.stream.next(false) {
-                        if HEX_CHARS.iter().any(|a| *a == self.stream.last_char()) {
-                            number.push(self.stream.last_char());
-                        }
-                        else {
-                            break;
-                        }
-                    }
-                    
-                    self.tokens.push(Token::Int(i64::from_str_radix(&number, 16).unwrap()));
-                }
-                else {
-                    let mut number = self.stream.last_char().to_string();
-                    let mut has_dot = false;
-                    let mut has_exponent = false;
-                    while self.stream.next(false) {
-                        if self.stream.last_char().is_digit(10) {
-                            number.push(self.stream.last_char());
-                        }
-                        else if !has_dot && self.stream.last_char() == '.' {
-                            has_dot = true;
-                            number.push(self.stream.last_char());
-                        }
-                        else if !has_exponent && self.stream.last_char().to_ascii_lowercase() == 'e' {
-                            has_exponent = true;
-                            number.push(self.stream.last_char());
-                            if self.stream.look(1).unwrap_or('\0') == '-' {
-                                self.stream.next(false);
-                                number.push('-');
-                            }
-                        }
-                        else {
-                            break;
-                        }
-                    }
-    
-                    match has_dot || has_exponent {
-                        true => self.tokens.push(Token::Real(number.parse().unwrap())),
-                        false => self.tokens.push(Token::Int(number.parse().unwrap())),
-                    }
-                }
+                self.process_number();
             }
             else if let Some(token) = OTHER_TOKENS.iter().find(|a| self.stream.look_for(&a, true)) {
                 self.tokens.push(Token::Other(token.to_string()));
@@ -203,6 +93,133 @@ impl Lexer {
             else {
                 self.error(&format!("Unknown character '{}'", self.stream.last_char()));
             }
+        }
+    }
+
+    fn process_block_comment(&mut self) {
+        let mut depth = 1;
+        while depth > 0 {
+            if self.stream.look_for("--]]", true) {
+                depth -= 1;
+            }
+            else if self.stream.look_for("--[[", true) {
+                depth += 1;
+            }
+            self.stream.next(false);
+        }
+    }
+
+    fn process_line_comment(&mut self) {
+        while self.stream.last_char() != '\n' {
+            self.stream.next(false);
+        }
+    }
+
+    fn process_short_string_literal(&mut self) {
+        let mut string = String::new();
+        while self.stream.next(false) {
+            if self.stream.last_char() == '\\' {
+                self.stream.next(false);
+                string.push(self.stream.last_char());
+            }
+            else if self.stream.last_char() == '\'' || self.stream.last_char() == '"' {
+                self.stream.next(false);
+                break;
+            }
+            else {
+                string.push(self.stream.last_char());
+            }
+        }
+        self.tokens.push(Token::String(string));
+    }
+
+    fn process_identifier(&mut self) {
+        let mut name = self.stream.last_char().to_string();
+        while self.stream.next(false) {
+            if self.stream.last_char().is_alphanumeric() || self.stream.last_char() == '_' {
+                name.push(self.stream.last_char());
+            }
+            else {
+                break;
+            }
+        }
+
+        self.tokens.push(match name.as_str() {
+            "function" => Token::Function,
+            "end" => Token::End,
+
+            "if" => Token::If,
+            "then" => Token::Then,
+            "else" => Token::Else,
+            "elseif" => Token::ElseIf,
+
+            "for" => Token::For,
+            "while" => Token::While,
+            "do" => Token::Do,
+            "repeat" => Token::Repeat,
+            "until" => Token::Until,
+
+            "or" => Token::Or,
+            "and" => Token::And,
+            "not" => Token::Not,
+
+            "goto" => Token::Goto,
+            "break" => Token::Break,
+            "return" => Token::Return,
+
+            "in" => Token::In,
+            "local" => Token::Local,
+            "nil" => Token::Nil,
+            "true" => Token::True,
+            "false" => Token::False,
+
+            _ => Token::Identifier(name),
+        });
+    }
+
+    fn process_number(&mut self) {
+        if self.stream.look(1).unwrap_or('\0').to_ascii_lowercase() == 'x' {
+            self.stream.next(false);
+            
+            let mut number = String::new();
+            while self.stream.next(false) {
+                if HEX_CHARS.iter().any(|a| *a == self.stream.last_char()) {
+                    number.push(self.stream.last_char());
+                }
+                else {
+                    break;
+                }
+            }
+            
+            let num = i64::from_str_radix(&number, 16).unwrap();
+            self.tokens.push(Token::Number(num as f64));
+        }
+        else {
+            let mut number = self.stream.last_char().to_string();
+            let mut has_dot = false;
+            let mut has_exponent = false;
+            while self.stream.next(false) {
+                if self.stream.last_char().is_digit(10) {
+                    number.push(self.stream.last_char());
+                }
+                else if !has_dot && self.stream.last_char() == '.' {
+                    has_dot = true;
+                    number.push(self.stream.last_char());
+                }
+                else if !has_exponent && self.stream.last_char().to_ascii_lowercase() == 'e' {
+                    has_exponent = true;
+                    number.push(self.stream.last_char());
+                    if self.stream.look(1).unwrap_or('\0') == '-' {
+                        self.stream.next(false);
+                        number.push('-');
+                    }
+                }
+                else {
+                    break;
+                }
+            }
+
+            self.tokens.push(Token::Number(number.parse().unwrap()))
         }
     }
 
