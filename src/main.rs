4024f9f72438f71cb9195a66af7fae2ff19b1501
Vlad2001_MFS <kopytin.vladislav2001@yandex.ru>
2020-10-07 19:56:24 +0300

    add: test source for lexer

--- src/main.rs
+++ src/main.rs
@@ -5,7 +5,7 @@ mod lexer;
 pub use lexer::*;
 
 fn main() {
-    let test_source = std::fs::read_to_string("./test.lua").unwrap();
+    let test_source = std::fs::read_to_string("./test_lexer.lua").unwrap();
     let tokens = Lexer::parse(&test_source, "test_source");
     println!("token = [");
     for token in tokens.iter() {
