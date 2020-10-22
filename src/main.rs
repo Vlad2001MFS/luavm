f02f8fc118c1f06d45aecbdaaefbeb5e88f64592
Vlad2001_MFS <kopytin.vladislav2001@yandex.ru>
2020-10-22 14:43:56 +0300

    add base code for processing all official lua tests (tests are failure yet)

--- src/main.rs
+++ src/main.rs
@@ -4,42 +4,67 @@ pub mod lexer;
 pub mod parser;
 
 use lexer::{
-    Lexer, Token,
+    Lexer,
 };
 use parser::{
     Parser,
 };
+use std::{
+    path::{
+        Path, PathBuf,
+    },
+    fs::File,
+    io::Write,
+};
+
+fn run_tests(test_dir_path: &str) {
+    let result_dir = PathBuf::from(test_dir_path).join("../").join(Path::new(test_dir_path).file_name().unwrap().to_string_lossy().to_string() + "-result");
+
+    if let Ok(meta) = std::fs::metadata(&result_dir) {
+        if meta.is_dir() {
+            std::fs::remove_dir_all(&result_dir).unwrap();
+        }
+    }
+
+    std::fs::create_dir(&result_dir).unwrap();
+
+    for entry in std::fs::read_dir(test_dir_path).unwrap() {
+        let entry = entry.unwrap();
+        let path = entry.path();
+        let test_file_name = path.file_name().unwrap().to_str().unwrap();
+        let test_file_stem = path.file_stem().unwrap().to_str().unwrap();
+
+        if path.extension().unwrap() == "lua" {
+            let test_source = std::fs::read_to_string(&path).unwrap();
+
+            let tokens = Lexer::parse(&test_source, test_file_name);
+
+            let mut tokens_file = File::create(result_dir.join(test_file_stem.to_owned() + ".tokens.txt")).unwrap();
+            for token in tokens.iter() {
+                writeln!(tokens_file, "  {:?}", token.token()).unwrap();
+            }
+
+            let chunk = Parser::parse(tokens);
+
+            let ast_string: String = format!("{:#?}", chunk).lines().map(|line| {
+                let mut spaces_count = 0;
+                for ch in line.chars() {
+                    match ch {
+                        ' ' => spaces_count += 1,
+                        _ => break,
+                    }
+                }
+                " ".repeat(spaces_count / 2) + line.trim_start() + "\n"
+            }).collect();
+
+            let mut ast_file = File::create(result_dir.join(test_file_stem.to_owned() + ".ast.txt")).unwrap();
+            writeln!(ast_file, "{}", ast_string).unwrap();
+
+            println!("### TEST: {} --- OK ###", test_file_name);
+        }
+    }
+}
 
 fn main() {
-    let test_source = std::fs::read_to_string("./test_parser.lua").unwrap();
-
-    let tokens = Lexer::parse(&test_source, "test_parser");
-    if false {
-        println!("token = [");
-        for token in tokens.iter() {
-            println!("  {:?}", token.token());
-            if let Token::Break = token.token() {
-                let pointer = " ".repeat(token.begin_location().column() - 1) + &"^".repeat(token.end_location().column() - token.begin_location().column());
-                println!("{}:{}: DESC\n{}\n{}", token.begin_location().source_name(), token.begin_location(), token.begin_location().content(), pointer);
-            }
-        }
-        println!("]");
-    }
-
-    let chunk = Parser::parse(tokens);
-    if true {
-        let ast_string: String = format!("{:#?}", chunk).lines().map(|line| {
-            let mut spaces_count = 0;
-            for ch in line.chars() {
-                match ch {
-                    ' ' => spaces_count += 1,
-                    _ => break,
-                }
-            }
-            " ".repeat(spaces_count / 2) + line.trim_start() + "\n"
-        }).collect();
-        println!("### AST >>>");
-        println!("{}", ast_string);
-        println!("### AST <<<");
-    }
+    run_tests("./lua-5.4.1-tests");
 }
