01edfc44ad55782f017650d3171d9c371bdac4a4
Vlad2001_MFS <kopytin.vladislav2001@yandex.ru>
2020-10-22 20:37:04 +0300

    add possible to skip the tests

--- src/main.rs
+++ src/main.rs
@@ -28,40 +28,47 @@ fn run_tests(test_dir_path: &str) {
 
     std::fs::create_dir(&result_dir).unwrap();
 
+    let mut skip_test = true;
     for entry in std::fs::read_dir(test_dir_path).unwrap() {
         let entry = entry.unwrap();
         let path = entry.path();
         let test_file_name = path.file_name().unwrap().to_str().unwrap();
         let test_file_stem = path.file_stem().unwrap().to_str().unwrap();
 
-        if let Some(Some("lua")) = path.extension().map(|ext| ext.to_str()) {
-            let test_source_bytes = std::fs::read(&path).expect(&format!("Failed to load test '{}'", path.to_string_lossy()));
-            let test_source = String::from_utf8_lossy(&test_source_bytes).to_string();
+        if !skip_test {
+            if let Some(Some("lua")) = path.extension().map(|ext| ext.to_str()) {
+                let test_source_bytes = std::fs::read(&path).expect(&format!("Failed to load test '{}'", path.to_string_lossy()));
+                let test_source = String::from_utf8_lossy(&test_source_bytes).to_string();
 
-            let tokens = Lexer::parse(&test_source, test_file_name);
+                let tokens = Lexer::parse(&test_source, test_file_name);
 
-            let mut tokens_file = File::create(result_dir.join(test_file_stem.to_owned() + ".tokens.txt")).unwrap();
-            for token in tokens.iter() {
-                writeln!(tokens_file, "{:?}", token.token()).unwrap();
-            }
-
-            let chunk = Parser::parse(tokens);
-
-            let ast_string: String = format!("{:#?}", chunk).lines().map(|line| {
-                let mut spaces_count = 0;
-                for ch in line.chars() {
-                    match ch {
-                        ' ' => spaces_count += 1,
-                        _ => break,
-                    }
+                let mut tokens_file = File::create(result_dir.join(test_file_stem.to_owned() + ".tokens.txt")).unwrap();
+                for token in tokens.iter() {
+                    writeln!(tokens_file, "{:?}", token.token()).unwrap();
                 }
-                " ".repeat(spaces_count / 2) + line.trim_start() + "\n"
-            }).collect();
 
-            let mut ast_file = File::create(result_dir.join(test_file_stem.to_owned() + ".ast.txt")).unwrap();
-            writeln!(ast_file, "{}", ast_string).unwrap();
+                let chunk = Parser::parse(tokens);
 
-            println!("### TEST: {} --- OK ###", test_file_name);
+                let ast_string: String = format!("{:#?}", chunk).lines().map(|line| {
+                    let mut spaces_count = 0;
+                    for ch in line.chars() {
+                        match ch {
+                            ' ' => spaces_count += 1,
+                            _ => break,
+                        }
+                    }
+                    " ".repeat(spaces_count / 2) + line.trim_start() + "\n"
+                }).collect();
+
+                let mut ast_file = File::create(result_dir.join(test_file_stem.to_owned() + ".ast.txt")).unwrap();
+                writeln!(ast_file, "{}", ast_string).unwrap();
+
+                println!("### TEST: {} --- OK ###", test_file_name);
+            }
+        }
+
+        if test_file_stem == "heavy" {
+            skip_test = false;
         }
     }
 }
